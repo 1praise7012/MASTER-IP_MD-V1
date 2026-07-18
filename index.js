2e82e59cbda21bccf92d2a56e0c8935b7f9bba86
MASTER-IP <masteripmaushe@gmail.com>
2026-07-18 22:48:55 +0200

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  menu: require('./menu'),
-  play: require('./play'),
-  play2: require('./play2'),
-  song: require('./song'),
-  video: require('./video'),
-  vv: require('./vv')
-}
